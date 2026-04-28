459d38d5fc43b8a0150bc5f0bf8d4e2093796a07
BaguioDaniel <work.danielbaguio@gmail.com>
2026-04-28 19:46:05 +0800

    Commit #9 | Adjusted naming convention and data
    Changed GPT to Gpt and adjusted achievements to my new resume

--- Portfolio/src/data/experiences.js
+++ Portfolio/src/data/experiences.js
@@ -1,6 +1,6 @@
 import resume from '../assets/Daniel Jeremiah Baguio.pdf';
 import aws_certificate from '../assets/AWS Cloud Practitioner Essentials.pdf';
-import chatgpt_certificate from '../assets/ChatGPT Certification.jpg';
+import chatgpt_certificate from '../assets/ChatGpt Certification.jpg';
 
 export const experiences = [
   {
@@ -18,10 +18,10 @@ export const experiences = [
     role: "Software Engineer",
     impact: "Improved user experience, productivity, and system performance",
     achievements: [
-      "Enhanced an existing web-based system by implementing new features, improving user productivity by 15% and streamlining daily operations.",
-      "Built and tested 5 features in simulated virtual machine environments to replicate production data scenarios, improving testing reliability",
-      "Collaborated with cross-functional teams, across 4 different projects, to deploy new system features and updates in production environments",
-      "Contributed to system stability and performance through structured testing and debugging reducing discrepancies and downtime by 9%"
+      "Delivered end-to-end full-stack development enhancement owning architecture, development, testing, and development, resulting in a 15% increase in user productivity and streamlined operations ",
+      "Designed and implemented backend processes using .NET, C#, SQL, and JavaScript, continuously refining technical implementations to match evolving business requirements. ",
+      "Executed 5 production deployments throughout 2025 which included post deployment support for critical releases, and supports that lasted 6 months due to scale and impact.  ",
+      " Improved system stability and performance by reducing discrepancies and downtime by 9% through structured testing, debugging, and validation in simulated VM environments, while collaborating across 4 cross-functional project teams. "
     ]
   },
   {
